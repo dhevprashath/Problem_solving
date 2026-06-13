f0470e1dc8359606ca14ec7c78259169a8e89e2c
dhevprashath <dhevprashath@gmail.com>
2026-06-13 15:10:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-const increasebtn = document.getElementById("incrementBtn");
-const decreasebtn = document.getElementById("decrementBtn");
-const resetbtn = document.getElementById("resetBtn");
-const countlabel = document.getElementById("count");
-let count = 0;
+let minNum = 1;
+let maxNum = 10;
+let randomNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-increasebtn.onclick = function(){
-    count ++;
-    countlabel.textContent =count;
-}
-decreasebtn.onclick = function(){
-    count --;
-    countlabel.textContent =count;
+let attempts = 0;
+let guess;
+let running = true;
+
+while (running) {
+    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}:`);
+    guess = Number(guess);
+
+   if (isNaN(guess)) {
+        window.alert("Please enter a valid number.");
+        
+    }
+    else if (guess > randomNum) {
+        window.alert("Too high! Try again.");
+        
+    }
+    else{
+        attempts++;
+        if (guess < randomNum) {
+            window.alert("Too low! Try again.");
+    }
+        else if ( guess > randomNum ) {
+            window.alert("Too high! Try again.");
+        }
+        else {
+            window.alert(`Congratulations! You've guessed the number ${randomNum} in ${attempts} attempts.`);
+            running = false;
+        }
+           
+    
 
 }
-resetbtn.onclick = function(){
-    count = 0;
-    countlabel.textContent =count;
+running = false;
 }
